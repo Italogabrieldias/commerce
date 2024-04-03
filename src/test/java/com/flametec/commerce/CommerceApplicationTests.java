c17eed3a4449f8c841576b823a683b4e8a6d4be2
Italogabrieldias <italogabrielde@gmail.com>
2024-04-02 21:56:13 -0300

    created the new project

---
+++ src/test/java/com/flametec/commerce/CommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flametec.commerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
