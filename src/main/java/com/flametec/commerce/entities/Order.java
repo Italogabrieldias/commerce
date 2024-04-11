17944177e41889b7ce898897c0d07af884e9239b
Italogabrieldias <italogabrielde@gmail.com>
2024-04-10 22:41:52 -0300

    OrderItem, OrderItemPK implement

--- src/main/java/com/flametec/commerce/entities/Order.java
+++ src/main/java/com/flametec/commerce/entities/Order.java
@@ -3,6 +3,9 @@ package com.flametec.commerce.entities;
 import jakarta.persistence.*;
 
 import java.time.Instant;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 @Entity
 @Table(name = "tb_order")
@@ -19,6 +22,9 @@ public class Order {
     private User client;
     @OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
     private Payment payment;
+
+    @OneToMany(mappedBy = "id.order")
+    private Set<OrderItem> items = new HashSet<>();
 
     public Order(){}
 
@@ -69,4 +75,12 @@ public class Order {
     public void setPayment(Payment payment) {
         this.payment = payment;
     }
+
+    public Set<OrderItem> getItems() {
+        return items;
+    }
+    public List<Product> getProduct(){
+        return items.stream().map( x -> x.getProduct()).toList();
+    }
+
 }
